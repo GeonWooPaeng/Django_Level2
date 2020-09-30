9f611c864edfe459b25dec697afc41b6d0b590a1
GeonWooPaeng <gwpaeng@naver.com>
2020-09-30 21:33:49 +0900

    상품 목록 만들기

--- fc_django/product/views.py
+++ fc_django/product/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView 
+from .models import Product 
 # Create your views here.
+
+class ProductList(ListView):
+    model = Product 
+    template_name = 'product.html' #template 지정
+    context_object_name = 'product_list' #object_list 지정
